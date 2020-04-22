8faca6314d1381ef9dc77ef7955871d387becb8a
Czeksu <camiloczekaj@gmail.com>
2020-04-22 13:04:58 +0200

    init empty branch

---
+++ src/Countries.vue
@@ -0,0 +1,22 @@
+<template>
+    <div class="row">
+          <h1>XD</h1>
+      </div>
+</template>
+
+<script>
+export default {
+  data () {
+    return {
+      countries: []
+    };
+  },
+    created: function() {
+      fetch("https://api.covid19api.com/summary")
+      .then(response => response.json())
+      .then((data) =>{
+        this.countries = data;
+      })
+    }
+  };
+</script>
