be5b7326d52bff828d0141f881da23b19b0d71ec
Czeksu <camiloczekaj@gmail.com>
2020-04-22 12:43:06 +0200

    Create Info.vue

---
+++ src/Info.vue
@@ -0,0 +1,22 @@
+<template>
+    <div class="row">
+          <h1> XDD </h1>
+      </div>
+</template>
+
+<script>
+export default {
+  data () {
+    return {
+      countries: []
+    };
+  },
+    created: function() {
+      fetch("https://api.covid19api.com/summary")
+      .then(response => response.json())
+      .then((data) =>{
+        this.countries = data;
+      })
+    }
+  }
+</script>
